89f08fe4663067b7c8107d83329588b4662b92fc
unknown <wjdfoek3@knu.ac.kr>
2024-12-15 00:43:38 +0900

    Feat: 회원가입 기능

--- front/src/page/Register.jsx
+++ front/src/page/Register.jsx
@@ -1,11 +1,43 @@
+import { useNavigate } from "react-router-dom"
 
 export default function Register() {
+    const navigate = useNavigate();
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
+        const id = event.target.ID.value;
+        const password = event.target.password.value;
+
+        try {
+            const response = fetch('http://localhost:8080/api/member/register', {
+                method : "POST",
+                headers : {
+                    'Content-type' : 'application/json',
+                },
+                body : JSON.stringify({
+                    "member_id" : id,
+                    "password" : password
+                }),
+            }).then((response) => {
+                if (response.ok) {
+                    alert("회원 가입 성공!");
+                    navigate('/login');
+                }
+                else {
+                    alert(id+"는 이미 존재합니다.");
+                }
+            })
+        } catch {
+            alert("Sign up failed! Try it again.");
+        }
+    }
 
     return (
-        <div>
-            <h1>회원가입페이지입니다.</h1>
-        </div>
+        <form onSubmit = { handleSubmit }>
+            <input id = 'ID' type="text"></input>
+            <input id = 'password' type='password'></input>
+            <button type="submit">제출</button>
+        </form>
     )
 }
