bb232fa718ca18eebda0e665fb9fc0f107f912dd
unknown <wjdfoek3@knu.ac.kr>
2024-12-15 14:17:47 +0900

    Feat: main 페이지 게시판 기능

--- front/src/page/Main.jsx
+++ front/src/page/Main.jsx
@@ -1,7 +1,64 @@
-export default function main() {
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"
+
+export default function Main() {
+    const navigate = useNavigate();
+    const [articleList, setArticleList] = useState([]);
+    const result = [];
+
+    async function getArticle() {
+        const response = await fetch('http://localhost:8080/api/article/', {
+            method : 'GET'
+        }).then(async (response) => {
+            try {
+                if (response.ok) {
+                    const data = await response.json(); // ArticleList
+                    
+                    const result = data.article_list.map((article) => ({
+                        article_id: article.id,
+                        owner_id: article.owner_id,
+                        title: article.title,
+                        image: article.image,
+                        text: article.text,
+                        time: article.time,
+                    }));
+
+                    setArticleList(result);
+                }
+            } catch (error) {
+                console.log("데이터 로딩 오류", error);
+            }
+        })
+    }
+
+    useEffect(() => {
+        getArticle();
+    },[]);
+
+    console.log(articleList.length);
+    for (let i = 0; i< articleList.length; i++) {
+        console.log(
+            articleList[i].article_id, articleList[i].title
+        );
+    }
+
+    const chunnkedArticles = [];
+    for (let i = 0; i < articleList.length; i += 3) {
+        chunnkedArticles.push(articleList.slice(i, i+3));
+    }
 
 
     return (
-        <h1>Hi</h1>
+        <div className = "board">
+            {chunnkedArticles.map((row, rowIndex) => (
+                <div className = "row" key = {rowIndex}>
+                    {row.map((article, articleIndex) => (
+                        <div className = "article" key = {articleIndex}>
+                            <h3>{article.title}</h3>
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </div>
     )
 }
--- front/src/page/MyPage.jsx
+++ front/src/page/MyPage.jsx
@@ -1,9 +1,20 @@
 
 export default function MyPage() {
+    const id = sessionStorage.getItem('member_id');
+
+    async function getMapList(id) {
+        // try {
+        //     const response = fetch('http://localhost:8080/article')
+        // }
+    }
 
     return (
         <div>
-            <h1>마이페이지입니다.</h1>
+            <h4>{id}</h4>
+            <button onClick = {console.log("hi")}>ID 변경</button>
+            <div>
+                {/* 게시글 리스트 보여주기 */}
+            </div>
         </div>
     )
 }
